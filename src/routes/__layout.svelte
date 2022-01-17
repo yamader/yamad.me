62ce4b09dbcb4275f45f6eaacc8eb98e3b28b8e7
YamaD <me@dyama.net>
2022-01-17 19:37:30 +0900

    scss

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,25 +1,26 @@
 <script lang="ts">
-  import "../app.css"
+  import "../app.scss"
 </script>
 
-<div class="container flex flex-col">
-  <header class="w-full">
-    <nav>
-      <ul class="flex list-none">
-        <li>
-          <a svelte:prefetch href="/">Top</a>
-        </li>
-        <li>
-          <a svelte:prefetch href="/about/">About</a>
-        </li>
-      </ul>
-    </nav>
-  </header>
-  <main class="">
-    <slot />
-  </main>
-  <footer class="">
-    <small class="inline-block mx-2 my-4 font-bold text-gray-600"
-    >Copyright &copy; 2020-{new Date().getFullYear()} YamaD</small>
-  </footer>
-</div>
+<header>
+  <nav class="headerNav">
+    <ul>
+      <li>
+        <a svelte:prefetch href="/">Top</a>
+      </li>
+      <li>
+        <a svelte:prefetch href="/about/">About</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<main>
+  <slot />
+</main>
+
+<footer>
+  <small class="copyright"
+    >Copyright &copy; 2020-{new Date().getFullYear()} YamaD</small
+  >
+</footer>
