4a6b0862e7ddbbd438ab4f09ac8f32e896ebae05
YamaD <me@dyama.net>
2022-01-27 18:56:21 +0900

    add cssnano

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,12 @@
 const autoprefixer = require("autoprefixer")
+const cssnano = require("cssnano")
+
+const mode = process.env.NODE_ENV
+const dev = mode === "development"
 
 module.exports = {
-  plugins: [autoprefixer],
+  plugins: [
+    autoprefixer,
+    !dev && cssnano({ preset: "default" }),
+  ],
 }
