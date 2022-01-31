eccd8ac2546cdbb5c4e8f0f01b670eca8037c3d3
YamaD <me@dyama.net>
2022-01-31 22:22:36 +0900

    CSSの整理・微調整

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -24,16 +24,125 @@
       </ul>
     </nav>
   </header>
-
   <hr />
-
   <main>
     <slot />
   </main>
-
   <hr />
-
   <footer>
     <small class="copyright">Copyright &copy; 2020-{new Date().getFullYear()} YamaD</small>
   </footer>
 </div>
+
+<style lang="scss">
+  @import "../styles/responsive.scss";
+  @import "../styles/colors.scss";
+  /* Container */
+  .container {
+    display: flex;
+    margin: 32px auto;
+    padding: 16px 32px;
+    max-width: 960px;
+    flex-direction: column;
+    color: black;
+    background: white;
+    border: 4px solid;
+    border-radius: 8px;
+    @include bpMqUp(lg) {
+      margin: 0;
+      border: none;
+    }
+    @include bpMqUp(sm) {
+      padding: 5%;
+      max-width: 100%;
+    }
+    & > hr {
+      margin: 0.8rem auto;
+      width: 80%;
+      @include bpMqUp(lg) {
+        width: 95%;
+      }
+      @include bpMqUp(md) {
+        width: 100%;
+      }
+    }
+  }
+  /* Header */
+  .headerNav {
+    display: flex;
+    margin: 1rem auto;
+    user-select: none;
+  }
+  .headerLinks {
+    display: flex;
+    margin: 0 auto;
+    line-height: 2em;
+    font-size: 1.1rem;
+    font-weight: bold;
+    &::before {
+      margin: auto 1.5em auto 0;
+      content: "->";
+      @include bpMqUp(sm) {
+        display: none;
+      }
+    }
+    &::after {
+      margin: auto 0 auto 1.5em;
+      content: "<-";
+      @include bpMqUp(sm) {
+        display: none;
+      }
+    }
+    li > a {
+      display: inline-block;
+      min-width: 6em;
+      color: black;
+      text-align: center;
+      text-decoration: none;
+      &:hover,
+      &:active,
+      &.active {
+        font-weight: bolder;
+        text-decoration: underline;
+      }
+      @include bpMqUp(md) {
+        min-width: 4em;
+      }
+      @include bpMqUp(sm) {
+        min-width: 0;
+      }
+    }
+    li + li::before {
+      margin: 0 1em;
+      content: "|";
+      @include bpMqUp(sm) {
+        margin: 0 .8em;
+      }
+    }
+  }
+  /* Main */
+  main {
+    margin-bottom: 2rem;
+    padding: 0 15%;
+    @include bpMqUp(xl) {
+      padding: 0 12%;
+    }
+    @include bpMqUp(lg) {
+      padding: 0 10%;
+    }
+    @include bpMqUp(md) {
+      padding: 0 5%;
+    }
+    @include bpMqUp(sm) {
+      padding: 0;
+    }
+  }
+  /* Footer */
+  .copyright {
+    display: block;
+    margin: 1em;
+    color: $c-dimText;
+    text-align: center;
+    font-weight: bold;
+  }
+</style>
