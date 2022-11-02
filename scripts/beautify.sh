0615c34fc028af8e76863976a235827ba9f7cd53
YamaD <me@dyama.net>
2022-11-02 22:26:13 +0900

    fix

--- scripts/beautify.sh
+++ scripts/beautify.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -e
+shopt -s globstar
 
 BEAUTIFY_OPTS="-s 2 -E [] --no-preserve-newlines"
 
-for f in build/*.html build/**/*.html; do
+for f in build/**/*.html; do
   t=`mktemp`
   echo "beautify $t -> $f"
-  sed 's/<!--.*-->//g' $f > $t
+  perl -pe 's/<!--.*?-->//g' $f > $t
   html-beautify $BEAUTIFY_OPTS $t > $f
   rm $t
 done
